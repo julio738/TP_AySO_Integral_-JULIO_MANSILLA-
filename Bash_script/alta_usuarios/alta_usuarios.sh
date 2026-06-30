655df3c3505edc942ea1c2f359a9232d17e98449
julio738 <mansillajulio.utn@gmail.com>
2026-06-29 21:38:36 -0300

    corrigiendo bash

--- Bash_script/alta_usuarios/alta_usuarios.sh
+++ Bash_script/alta_usuarios/alta_usuarios.sh
@@ -2,26 +2,26 @@
 clear
 
 ###############################
-#
 # Parametros:
-#  - Lista de Usuarios a crear
+#  - Lista de Usuarios a crear (usuario,grupo,home)
 #  - Usuario del cual se obtendra la clave
-#
-#  Tareas:
-#  - Crear los usuarios segun la lista recibida en los grupos descriptos
-#  - Los usuarios deberan de tener la misma clave que la del usuario pasado por parametro
-#
 ###############################
 
 LISTA=$1
+USUARIO_BASE=$2
+
+# Obtener el hash de la clave del usuario base
+CLAVE=$(sudo grep "^$USUARIO_BASE:" /etc/shadow | awk -F ':' '{print $2}')
 
 ANT_IFS=$IFS
 IFS=$'\n'
-for LINEA in `cat $LISTA |  grep -v ^#`
+for LINEA in $(grep -v ^# "$LISTA")
 do
-	USUARIO=$(echo  $LINEA |awk -F ':' '{print $1}')
-	GRUPO=$(echo  $LINEA |awk -F ':' '{print $2}')
-	echo "sudo useradd -m -s /bin/bash -g $GRUPO $USUARIO"
+    USUARIO=$(echo "$LINEA" | awk -F ',' '{print $1}')
+    GRUPO=$(echo "$LINEA"   | awk -F ',' '{print $2}')
+    HOME=$(echo "$LINEA"    | awk -F ',' '{print $3}')
+
+    # Crear usuario con grupo, home y misma clave
+    sudo useradd -m -s /bin/bash -g "$GRUPO" -d "$HOME" -p "$CLAVE" "$USUARIO"
 done
 IFS=$ANT_IFS
-
